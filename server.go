eb29df3377588eab04b1f11ebc4134749ceaa419
Kartik <kartik@hyperverge.co>
2021-08-14 23:59:52 +0530

    Add: controller

--- server.go
+++ server.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gogin-poc/controller"
+	"gogin-poc/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/api", func(ctx *gin.Context) {
+	server.GET("/posts", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/post", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "AoK",
 		})
